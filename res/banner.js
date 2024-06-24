5f4344a40313c31a0e6421dbf3c2efc95ea976dc
ranielhiyan <ranielhiyan184@gmail.com>
2024-06-25 01:53:36 +0800

    update

---
+++ res/banner.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const donateBanner = document.getElementById('donateBanner');
+    const donateButton = document.getElementById('donateButton');
+    const closeButton = document.getElementById('closeButton');
+    let deferredPrompt;
+    setTimeout(() => {
+        donateBanner.style.display = 'block';
+    }, 4000);
+    donateButton.addEventListener('click', () => {
+        window.location.href = 'https://www.facebook.com/profile.php?id=61555221003228';
+    });
+    closeButton.addEventListener('click', () => {
+        donateBanner.style.display = 'none';
+    });
+});
