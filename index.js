0981361b565e1c250938ca11cdcac9a6ac94e784
ranielhiyan <ranielhiyan184@gmail.com>
2024-07-09 10:03:52 +0800

    update

--- index.js
+++ index.js
@@ -16,11 +16,6 @@ app.listen(PORT, () => {
 });
 
 
-let $name = "dako bayag oten si raniel 7.9 inches "
 
-alert ($name);
-
-let $me = " gusto ka mo tan aw? pm sa peysbok "
-alert ($me);
 
 // Pa repaste
