036bc687c857972f74b8ba10c39844de4575a1ed
ranielhiyan <ranielhiyan184@gmail.com>
2024-07-09 09:59:47 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 
-let $name = "dako bayag oten si raniel 7.9 inches "
-
-alert ($name);;
-
-let $me = " gusto ka mo tan aw? pm sa peysbok "
-alert ($me);
 
 
 const express = require('express');
@@ -21,5 +15,12 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+
+let $name = "dako bayag oten si raniel 7.9 inches "
+
+alert ($name);
+
+let $me = " gusto ka mo tan aw? pm sa peysbok "
+alert ($me);
 
 // Pa repaste
