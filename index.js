91fa18e1e4306b68e151858e36e05ca5b6620347
ranielhiyan <ranielhiyan184@gmail.com>
2024-07-17 22:39:47 +0800

    update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 20797;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
